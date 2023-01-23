c6062f9165e881341f1726665b64bfe6d5ff1a8b
Gislaine de Aguiar Guimaraes <gis_guimaraes@hotmail.com>
2023-01-23 09:36:33 +0100

    ajout cors

--- src/app.js
+++ src/app.js
@@ -4,13 +4,19 @@ const app = express()
 //modifie les informations en fonction de l'environnement
 require('dotenv').config()
 
-//cors dit qui est autorisé à interroger notre API
-const cors = require('cors')
-
 app.use(express.json())
 
 //Import Route
 const argonauteRoutes = require('./routes/argonaute')
+
+//Middleware CORS dit qui est autorisé à interroger notre API
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader("Cross-Origin-Resource-Policy", "same-site");
+    next();
+});
 
 // Load the route
 app.use('/api/argonaute', argonauteRoutes)
